f347933623d7ba7044036f2a3316130c38418f1e
yunvana <yunvana@naver.com>
2016-10-21 14:49:07 +0900

    add login

--- mysite/settings.py
+++ mysite/settings.py
@@ -70,7 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
@@ -120,3 +119,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+LOGIN_REDIRECT_URL = '/'
